065fb1e4936c72c268b0b8211ef426500ad8c7ec
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2022-07-10 12:57:48 +0530

    Write an integration test

--- src/test/java/com/example/cosmos/EmployeeServiceApplicationTests.java
+++ src/test/java/com/example/cosmos/EmployeeServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.cosmos;
 
 import com.example.cosmos.config.DatabaseAndContextConfiguration;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class EmployeeServiceApplicationTests extends DatabaseAndContextConfiguration {
 
-	@Test
-	void contextLoads() {
+    @Autowired
+    private MockMvc mockMvc;
 
-	}
+    @Autowired
+    private ObjectMapper mapper;
 
+    @Test
+    void shouldSave() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(post("/employees").content("{\n" +
+                                "\t\"name\": \"Vibe\",\n" +
+                                "\t\"city\": \"Bangalore\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        JsonNode response = mapper.readValue(mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8), JsonNode.class);
+        assertNotNull(UUID.fromString(response.get("id").asText()));
+    }
 }
--- src/test/java/com/example/cosmos/config/DatabaseAndContextConfiguration.java
+++ src/test/java/com/example/cosmos/config/DatabaseAndContextConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.cosmos.config;
 
 import com.example.cosmos.EmployeeServiceApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {EmployeeServiceApplication.class, CosmosTestConfg.class})
+@AutoConfigureMockMvc
 public abstract class DatabaseAndContextConfiguration {
 }
